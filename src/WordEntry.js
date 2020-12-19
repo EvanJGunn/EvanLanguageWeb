e738fef801712e2c9021faa4fe0e18dd449ba9c9
Evan Gunn <egunn93@gmail.com>
2020-12-18 22:38:06 -0600

    Added word entry class

---
+++ src/WordEntry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WordEntry extends React.Component {
+  render() {
+    return (
+      <div className = "word-list">
+        <h1> Enter Word: </h1>
+      </div>
+    )
+  }
+}
+
+export default WordEntry;
